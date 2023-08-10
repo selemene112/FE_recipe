ab4c448839d904fee1422225ecc2e33b714a2fe5
selemene112 <ariyanda98y@gmail.com>
2023-08-10 08:08:39 +0800

    UPDATE(V1): ADD Filtur Login

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import viteLogo from '/vite.svg';
 import Menu from './pages/DetailRecipe';
 import InputMenu from './pages/inputMenu';
 import EditMenuPage from './pages/EditMenu';
+import Login from './pages/Login';
 
 function Menu1() {
   const { id } = useParams();
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/Menu" replace={true} />} />
           <Route path="/Menu" element={<Menu />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/InputMenu" element={<InputMenu />} />
           <Route path="/EditMenuPage/:id" element={<EditMenuPage />} />
 
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [inputData, setInputData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputData({
+      ...inputData,
+      [name]: value,
+    });
+  };
+
+  const AuthLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/user/login', inputData);
+      console.log(response.data); // Data dari respons
+      // Lakukan tindakan lanjutan setelah berhasil masuk
+      navigate('/'); // Contoh: Arahkan ke dashboard setelah masuk berhasil
+    } catch (error) {
+      console.error('Error:', error);
+      // Lakukan penanganan kesalahan, misalnya menampilkan pesan kesalahan ke pengguna
+    }
+  };
+
+  return (
+    <div>
+      <section className="container w-100">
+        <div className="w-100 my-5 registration d-flex align-items-center justify-content-center">
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div className="mb-5 auth-desc text-center">
+              <h2 className="mb-5 fw-bolder">Recipe</h2>
+              <h1>Welcome</h1>
+              <p>Login into your existing account</p>
+            </div>
+            <hr />
+            <form onSubmit={AuthLogin}>
+              <label htmlFor="email" className="mt-3">
+                Email
+              </label>
+              <input type="text" id="email" name="email" value={inputData.email} onChange={handleInputChange} placeholder="Enter email address" className="p-3 rounded w-100 mt-3" />
+              <label htmlFor="password" className="mt-3">
+                Password
+              </label>
+              <input type="password" id="password" name="password" value={inputData.password} onChange={handleInputChange} placeholder="Password" className="p-3 rounded w-100 mt-3" />
+              <label className="check-wrap mt-3">
+                I agree to terms & conditions
+                <input type="checkbox" checked="checked" />
+                <span className="checkmark"></span>
+              </label>
+              <button type="submit" className="p-3 border-0 bg-warning text-white rounded mt-5 w-100">
+                Login
+              </button>
+            </form>
+            <div className="mt-1">
+              <p>
+                Forgot your password?{' '}
+                <a href="./forget_password.html" className="text-decoration-none text-warning">
+                  Click here
+                </a>
+              </p>
+            </div>
+            <div className="mt-5 text-center">
+              <p>
+                Don't have an account?{' '}
+                <a href="./register.html" className="text-decoration-none text-warning">
+                  Sign Up
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
